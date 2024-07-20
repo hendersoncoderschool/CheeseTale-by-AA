cbee67b92b293fcd285f9a12b5c99a1a0d2530db
Henderson The Coder School <caleb.scott0125@gmail.com>
2024-07-20 12:57:26 -0700

    directional animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    
+    private Animator animator;
+    private string direction = ""; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,22 +18,50 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey("w"))
         {
+            if (direction != "up")
+            {
+                animator.SetTrigger("up");
+                direction = "up";
+            }
+
             transform.Translate(transform.up * speed * Time.deltaTime);
         }
 
         if (Input.GetKey("s"))
         {
+            if(direction != "down")
+            {
+                animator.SetTrigger("down");
+                direction = "down";
+            }
+            
             transform.Translate(-transform.up * speed * Time.deltaTime);
+                
         }
 
         if (Input.GetKey("a"))
         {
+            if (direction != "left")
+            {
+                animator.SetTrigger("left");
+                direction = "left";
+            }
             transform.Translate(-transform.right * speed * Time.deltaTime);
         }
 
         if (Input.GetKey("d"))
         {
+            if (direction != "right")
+            {
+                animator.SetTrigger("right");
+                direction = "right";
+            }
             transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyUp("w") ||  Input.GetKeyUp("a") ||  Input.GetKeyUp("s") || Input.GetKeyUp("d")){
+            animator.SetTrigger("idle");
+            direction = "";
         }
     }
 }
