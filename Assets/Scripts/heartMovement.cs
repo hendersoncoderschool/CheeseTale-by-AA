8f0c3c51c02e676c2e04263c1d7743dc941a23b9
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-08-17 13:00:28 -0700

    battle movement and ui

---
+++ Assets/Scripts/heartMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class heartMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
+
+
